b71b66b4748cefa64c2ea44ed2b04662244df03c
Nariman <abonent26pg@mail.ru>
2023-02-23 20:55:12 +0300

    added details(solo) page

--- src/store/root-reduce.js
+++ src/store/root-reduce.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import { themeReducer } from "./theme/theme-reducer";
 import { countriesReducer } from "./countries/countries-reducer";
 import { controlsReducer } from "./controls/controls-reducer";
+import { detailsReducer } from "./details/details-reducer";
 export const rootReducer = combineReducers({
   theme: themeReducer,
   countries: countriesReducer,
   controls: controlsReducer,
+  details: detailsReducer,
 });
